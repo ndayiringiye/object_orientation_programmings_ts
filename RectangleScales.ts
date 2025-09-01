4c016ff786b65fd90a517fedffaf4af56fefcc7e
ndayiringiye <ndayringiyedavid120@gmail.com>
2025-09-01 13:29:02 +0200

    chore:/object orientation programming find rectangle area and perimenter successfully

---
+++ RectangleScales.ts
@@ -0,0 +1,22 @@
+class Rectangle {
+  private width: number;
+  private height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+
+  perimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+
+const rect = new Rectangle(45, 67);
+
+console.log("Area:", rect.area());         
+console.log("Perimeter:", rect.perimeter()); 
